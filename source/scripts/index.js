b092123e004386e782f7034d173166172b4e7617
Peso4ek-program <79717976+Peso4ek-program@users.noreply.github.com>
2024-12-30 16:39:20 +0300

    #7 Декоративные элементы (#9)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
 /* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.main-nav__user');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav__user--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-nav__user--closed')) {
+    navMain.classList.remove('main-nav__user--closed');
+    navMain.classList.add('main-nav__user--opened');
+  } else {
+    navMain.classList.add('main-nav__user--closed');
+    navMain.classList.remove('main-nav__user--opened');
+  }
+});
